17b72bb7bc484c20bd7c0734a8becaaff3d1292a
JHugoSilva <hugosilvahs31@gmail.com>
2024-10-13 19:19:23 -0300

    Teste tag

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,6 +1,6 @@
 <script setup></script>
 <template>
     <header>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe molestiae recusandae dicta, ex neque veritatis natus! Voluptates ullam tempora repudiandae saepe cupiditate voluptate. Ipsum autem molestias ex voluptatum quasi ipsam.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing . Saepe molestiae recusandae dicta, ex neque veritatis natus! Voluptates ullam tempora repudiandae saepe cupiditate voluptate. Ipsum autem molestias ex voluptatum quasi ipsam.</p>
     </header>
 </template>
